fad41ab8414d48673b467ff7737a32792589c07d
Saquiba <saquibawasi@gmail.com>
2025-11-17 12:45:15 +0530

    ssa

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const path = require("path");
 const app = express();
 
 // ✅ Middleware setup
-const allowedOrigins = ["http://localhost:5173", "http://localhost:3000","http://localhost:3001", 'https://attendancefrontend.vercel.app'];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:3000","http://localhost:3001", 'https://attendancefrontend.vercel.app',"https://www.timelyhealth.in",
+  "https://timelyhealth.in"];
 
 app.use(
   cors({
