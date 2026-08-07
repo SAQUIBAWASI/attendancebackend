6984274351b549b58044a07bf0e3c8062937db29
JULEEP <juleeperween34@gmail.com>
2026-08-07 18:49:47 +0530

    qww

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const app = express();
 
 // ✅ Middleware setup
 const allowedOrigins = ["http://localhost:5173", "http://localhost:3000", "http://localhost:3001", 'https://attendancefrontend.vercel.app', "https://bm-frontend-lyart.vercel.app", "https://www.timelyhealth.in",
-  "https://timelyhealth.in", "http://62.72.29.27:3045", "https://taskmanagement.iryax.com"];
+  "https://timelyhealth.in", "http://62.72.29.27:3045", "https://taskmanagement.iryax.com", "https://ingrainhire.ingrainsystems.com"];
 
 app.use(
   cors({
